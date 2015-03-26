39bbbfc52a56f8f10f5ba03de1ee77c7c8db8219
GaryNg <garyng.zhongbo@gmail.com>
2015-03-27 00:50:31 +0800

    + Solve #11 and #42

---
+++ ID11/Program.cs
@@ -0,0 +1,38 @@
+﻿// Factorial - Number of zeros
+// 2015-03-27 00:28
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int caseNum = int.Parse(Console.ReadLine());
+			for (int i = 0; i < caseNum; i++)
+			{
+				string cases = Console.ReadLine();
+				int num = int.Parse(cases);
+
+				Console.WriteLine(noOfZeros(num));
+
+			}
+		}
+
+		static int noOfZeros(int num)
+		{
+			double n = num;
+			int noOf0 = 0;
+			while (Math.Floor(n / 5) > 0)
+			{
+				n = Math.Floor(n / 5);
+				noOf0 += (int)n;
+			}
+			return noOf0;
+		}
+	}
+}
---
+++ ID42/Program.cs
@@ -0,0 +1,35 @@
+﻿// Adding Reversed Numbers
+// 2015-03-27 00:29
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOJSol
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int caseNum = int.Parse(Console.ReadLine());
+			for (int i = 0; i < caseNum; i++)
+			{
+				string cases = Console.ReadLine();
+
+				string[] strCase = cases.Split(' ');
+				int sum = int.Parse(reverseString(strCase[0])) + int.Parse(reverseString(strCase[1]));
+				sum = int.Parse(reverseString(sum.ToString()));
+				Console.WriteLine(sum);
+			}
+		}
+
+		static string reverseString(string str)
+		{
+			char[] c = str.ToCharArray();
+			Array.Reverse(c);
+			return new string(c);
+		}
+
+	}
+}
